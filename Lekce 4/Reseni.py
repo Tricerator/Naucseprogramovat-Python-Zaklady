7940e7f8988d447cf3b16ee855c9bdad66f48d18
Tricerator <37005796+Tricerator@users.noreply.github.com>
2024-07-28 15:17:03 +0200

    Update Reseni.py

--- Lekce 4/Reseni.py
+++ Lekce 4/Reseni.py
@@ -1,5 +1,5 @@
 a = 10
 print(a % 2)
 
-Pokud je výsledek 0, číslo bylo sudé, pokud je výsledek 1, číslo bylo liché.
-Zkuste, zda to funguje i pro záporná čísla
+# Pokud je výsledek 0, číslo bylo sudé, pokud je výsledek 1, číslo bylo liché.
+# Zkuste, zda to funguje i pro záporná čísla
